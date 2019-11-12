bd21dba1bbd5cf963307b8e3a490bc951db99c0e
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2019-11-12 12:58:32 +0000

    Add dummy runtime lib

---
+++ runtime/dummy/prefetcher_dummy_rt.cpp
@@ -0,0 +1,125 @@
+//
+//
+//
+
+#include <stdio.h>
+// using fprintf
+
+#include <stdint.h>
+// using uintptr_t
+
+#include "pf_interface.h"
+
+extern "C" {
+
+int create_params(int num_nodes_pf, int num_edges_pf, int num_triggers_pf);
+int create_enable();
+int register_node(void *base, int64_t size, int64_t node_id);
+int register_node_with_size(void *base, int64_t size, int64_t elem_size,
+                            int64_t node_id);
+int register_trav_edge1(uintptr_t baseaddr_from, uintptr_t baseaddr_to,
+                       FuncId f);
+int register_trav_edge2(NodeId id_from, NodeId id_to, FuncId f);
+int register_trig_edge1(uintptr_t baseaddr_from, uintptr_t baseaddr_to, FuncId f,
+                       FuncId sq_f);
+int register_trig_edge2(NodeId id_from, NodeId id_to, FuncId f, FuncId sq_f);
+int sim_user_pf_set_param();
+int sim_user_pf_set_enable();
+int sim_user_pf_enable();
+int sim_user_wait();
+int sim_roi_start();
+int sim_roi_end();
+int sim_user_pf_disable();
+int delete_params();
+int delete_enable();
+
+} // extern "C"
+
+int create_params(int num_nodes_pf, int num_edges_pf, int num_triggers_pf) {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int create_enable() {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int register_node(void *base, int64_t size, int64_t node_id) {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int register_node_with_size(void *base, int64_t size, int64_t elem_size,
+                            int64_t node_id) {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int register_trav_edge1(uintptr_t baseaddr_from, uintptr_t baseaddr_to,
+                       FuncId f) {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int register_trav_edge2(NodeId id_from, NodeId id_to, FuncId f) {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int register_trig_edge1(uintptr_t baseaddr_from, uintptr_t baseaddr_to, FuncId f,
+                       FuncId sq_f) {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int register_trig_edge2(NodeId id_from, NodeId id_to, FuncId f, FuncId sq_f) {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int sim_user_pf_set_param() {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int sim_user_pf_set_enable() {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int sim_user_pf_enable() {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int sim_user_wait() {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int sim_roi_start() {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int sim_roi_end() {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int sim_user_pf_disable() {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int delete_params() {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
+int delete_enable() {
+  fprintf(stderr, "calling func: %s\n", __func__);
+  return 0;
+}
+
