0bfc2f3e138bffa75e32dca4e1b873bcc247dd33
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2019-11-12 08:56:55 +0000

    Add skeleton for prefetcher runtime

---
+++ runtime/default/prefetcher_rt.cpp
@@ -0,0 +1,21 @@
+//
+//
+//
+
+#include <stdlib.h>
+// using atexit
+// using abort
+// using getenv
+// using atol
+
+#include <stdio.h>
+// using fprintf
+// using fopen
+// using fclose
+
+extern "C" {
+void prefetcher_init(void);
+} // extern "C"
+
+void prefetcher_init(void) { return; }
+
