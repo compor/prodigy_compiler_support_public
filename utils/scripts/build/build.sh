81c7c3317024002c34ee5417cb63d6e32efb20db
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2021-02-04 08:59:58 +0000

    Add build scripts

---
+++ utils/scripts/build/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+PRJ_ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../" && pwd)"
+SRC_DIR=${1:-$PRJ_ROOT_DIR}
+INSTALL_DIR=${2:-../install/}
+
+#
+
+# Note: we require word splitting in CMAKE_OPTIONS, so leave it unquoted
+
+cmake \
+  -G Ninja \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
+  -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+  -DBUILD_SHARED_LIBS="${SHARED_LIBS}" \
+  -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" \
+  -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=On \
+  -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
+  ${CMAKE_OPTIONS} \
+  "${SRC_DIR}"
+
---
+++ utils/scripts/build/exports_deps1.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# LLVM/Clang should be in the PATH for this to work
+export CC=clang
+export CXX=clang++
+export LLVMCONFIG=llvm-config
+
+#COMPILER_VERSION=
+
+# or picked up from a system package install
+if [[ ! -z ${COMPILER_VERSION} ]]; then
+  export CC=${CC}-${COMPILER_VERSION}
+  export CXX=${CXX}-${COMPILER_VERSION}
+  export LLVMCONFIG=${LLVMCONFIG}-${COMPILER_VERSION}
+fi
+
+BUILD_TYPE=RelWithDebInfo
+export BUILD_TYPE
+
+SHARED_LIBS=ON
+export SHARED_LIBS
+
+CXX_FLAGS=
+CXX_FLAGS="${CXX_FLAGS} -O1"
+CXX_FLAGS="${CXX_FLAGS} -stdlib=libc++"
+CXX_FLAGS="${CXX_FLAGS} -pedantic -Wall -Wextra"
+CXX_FLAGS="${CXX_FLAGS} -Wno-unused-parameter -Wno-unused-function"
+export CXX_FLAGS
+
+LINKER_FLAGS=
+LINKER_FLAGS="${LINKER_FLAGS} -Wl,-L$(${LLVMCONFIG} --libdir)"
+LINKER_FLAGS="${LINKER_FLAGS} -lc++ -lc++abi"
+export LINKER_FLAGS
+
+# find LLVM's cmake dir
+LLVM_DIR=$(${LLVMCONFIG} --cmakedir)
+export LLVM_DIR
+
+CMAKE_OPTIONS=""
+
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DPCS_SNIPER_ROOT_DIR=/bulk/workbench/repos/commetc2.0_original/sniper6.1/"
+
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DLLVM_DIR=${LLVM_DIR}"
+
+# CXX lang standard options
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DCMAKE_CXX_STANDARD=14"
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DCXX_STANDARD_REQUIRED=ON"
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DCMAKE_CXX_EXTENSIONS=OFF"
+
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
+
+export CMAKE_OPTIONS
